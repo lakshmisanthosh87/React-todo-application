ba2572487e3dacb3231b500edbbf771942cc29af
lakshmisanthosh87 <lakshmisanthosh8714@gmail.com>
2025-12-29 10:56:51 +0530

    todocontext is created

---
+++ src/context/TodoContext.jsx
@@ -0,0 +1,38 @@
+import { Children, createContext,useEffect,useState } from "react";
+
+export const TodoContext = createContext();
+
+export const TodoProvider = ({Children})=>{
+    const [todos,setTodos] = useState(()=>{
+        const save = localStorage.getItem("todos")
+        return save ? JSON.parse(save) : []
+    }) 
+
+
+    useEffect(()=>{
+        localStorage.setItem("todos",JSON.stringify(todos))
+    })
+
+
+    const addTodo = (text)=>{
+        setTodos([...todos,{id:Date.now(),text}])
+    }
+
+
+    const deleteTodo = (id)=>{
+        setTodos(todos.filter(todo=>todo.id!==id))
+    }
+
+    const updateTodo = (id,newtext)=>{
+        setTodos(todos.map(todo=>todo.id==id?(...todo,text:newtext) : todo.text))
+
+    }
+
+    return(
+        <TodoContext.Provider value={{todos,addTodo,deleteTodo,updateTodo}}>
+            {Children}
+        </TodoContext.Provider>
+    )
+
+
+}
